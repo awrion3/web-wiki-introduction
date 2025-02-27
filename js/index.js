96e661f93c7fa1e419e41a14654549df260540df
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-27 14:02:33 +0900

    feat: add comment submit function

--- js/index.js
+++ js/index.js
@@ -1,19 +1,24 @@
-console.log(
-  "%c" +
-    " __      __  ______   __  __   ______     " +
-    "\n" +
-    "/\\ \\  __/\\ \\ /\\__  _\\ /\\ \\ /\\ \\ /\\__  _\\    " +
-    "\n" +
-    "\\ \\ \\/\\ \\ \\ \\/_\\/\\ \\/ \\ \\ \\/'/'\\/ _/\\ \\/    " +
-    "\n" +
-    " \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  \\ \\ , <    \\ \\ \\    " +
-    "\n" +
-    "  \\ \\ \\_/ \\_\\ \\ \\_\\ \\__\\ \\ \\\\`\\   \\_\\ \\__ " +
-    "\n" +
-    "   \\ `\\___x___/ /\\_____\\\\ \\_\\ \\_\\ /\\_____\\ " +
-    "\n" +
-    "    '/__//__/  /_____/ \\/_/\\/_/ /_____/",
-  "color: #d81b60; font-size: 16px; font-weight: bold;"
-);
+function submitComment() {
+        const commentText = document.querySelector("textarea").value;
 
-console.log("알맞은 스크립트를 작성하세요");
+        if (commentText.trim() !== "") {
+            const newComment = document.createElement("li");
+
+            newComment.innerHTML = `
+                <div class="comment-item">
+                <div class="comment-author">
+                <img src="./images/comment-author-icon.png" alt="사용자 프로필 이미지" />
+                <span>방문자</span>
+                </div>
+                <div class="comment-content">${commentText}</div>
+                </div>
+            `;
+
+            document.querySelector(".comment-list").appendChild(newComment);
+            document.querySelector("textarea").value = "";
+
+            alert("댓글이 등록되었습니다.");
+        } else {
+            alert("댓글을 입력해주세요.");
+        }
+}
